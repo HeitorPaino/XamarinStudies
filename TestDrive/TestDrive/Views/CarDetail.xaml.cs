4c2d2527b35a91cb6e4cd6a69a5958b3a4321ab4
heitorpaino <heitorpaino@gmail.com>
2017-05-16 17:37:08 -0300

    * packages.config: Gosh
    * CarList.xaml:
    * CarDetail.xaml:
    * CarList.xaml.cs:
    * CarDetail.xaml.cs:

---
+++ TestDrive/TestDrive/Views/CarDetail.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TestDrive.Views
+{
+	public partial class CarDetail : ContentPage
+	{
+		public Veiculo Veiculo { get; set; }
+
+        public CarDetail(Veiculo veiculo)
+		{
+			this.Veiculo = veiculo;
+			InitializeComponent();
+			this.BindingContext = this;
+		}
+
+		private void botaoProximo_Clicked(object sender, EventArgs e)
+		{
+            Navigation.PopAsync();
+		}
+	}
+}
